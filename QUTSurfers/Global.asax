e9e44dc19fdcfa63ffa2f551b5d52cf2f04da613
Erick Mejia <erick.mejia.au@gmail.com>
2019-02-02 11:41:28 +1000

    Add project files.

---
+++ QUTSurfers/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="QUTSurfers.MvcApplication" Language="C#" %>
---
+++ QUTSurfers/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(QUTSurfers.Startup))]
+namespace QUTSurfers
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
