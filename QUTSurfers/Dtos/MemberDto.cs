e6b045c8e758ec15803a24225bb378be66c8d32a
Erick Mejia <erick.mejia.au@gmail.com>
2019-02-08 09:21:42 +1000

    Adding MemberDto and Configuration of MappingProfile

---
+++ QUTSurfers/Dtos/MemberDto.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QUTSurfers.Dtos
+{
+    public class MemberDto
+    {
+        public int Id { get; set; }
+
+        public DateTime DateAdded { get; set; }
+        
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+        
+        public string Email { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string StudentNumber { get; set; }
+
+        public bool IntlStudent { get; set; }
+
+        public byte SurfingLevelId { get; set; }
+
+        public LevelOfSurfingDto SurfingLevel { get; set; }
+
+        public byte PaymentTypeId { get; set; }
+
+        public PaymentDto PaymentType { get; set; }
+
+        public bool PaymentApproval { get; set; }
+    }
+}
--- QUTSurfers/Global.asax.cs
+++ QUTSurfers/Global.asax.cs
@@ -6,6 +6,8 @@ using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 using System.Web.Http;
+using AutoMapper;
+using QUTSurfers.App_Start;
 
 namespace QUTSurfers
 {
@@ -13,7 +15,7 @@ namespace QUTSurfers
     {
         protected void Application_Start()
         {
-
+            Mapper.Initialize(c => c.AddProfile<MappingProfile>());
             GlobalConfiguration.Configure(WebApiConfig.Register);
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
